a7ef34131215b7ea27e0ba96ff334d3dc2caa1ee
Nirmal Shaju <alduinnirmal99@gmail.com>
2021-05-14 21:51:46 +0530

    First Commit

---
+++ app/src/main/java/com/nirmal/bookhub/DashboardFragment.kt
@@ -0,0 +1,27 @@
+package com.nirmal.bookhub
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class DashboardFragment : Fragment() {
+    lateinit var recyclerDashboard: RecyclerView
+    lateinit var layoutManager: RecyclerView.LayoutManager
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view =inflater.inflate(R.layout.fragment_dashboard, container, false)
+        recyclerDashboard = view.findViewById(R.id.recyclerDashboard)
+        layoutManager = LinearLayoutManager(activity)
+        return view
+    }
+
+}
